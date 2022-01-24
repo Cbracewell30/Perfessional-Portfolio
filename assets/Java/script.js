e8ddbe2a2f290938d75efbe5c3bd4cb3926944bf
Chris <chrisbracewell35@gmail.com>
2022-01-23 22:41:15 -0500

    adding new projects and jquery and bootstrap links

---
+++ assets/Java/script.js
@@ -0,0 +1,9 @@
+var headernav = document.querySelector(".navBtn");
+var headerEl = document.querySelector(".start-header");
+
+
+headernav.addEventListener("click", function() {
+headerEl.classList.add("opacity-25")
+});
+
+
